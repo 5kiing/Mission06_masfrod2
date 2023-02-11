d17c009ec26422da5e2c14a0ef4dbbbc9acf2c6c
5kiing <103718596+5kiing@users.noreply.github.com>
2023-02-11 13:41:34 -0700

    Add project files.

---
+++ Mission06_masfrod2/Models/AddMovieContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_masfrod2.Models
+{
+    public class AddMovieContext : DbContext
+    {
+        // constructor
+        public AddMovieContext(DbContextOptions<AddMovieContext> options) : base (options)
+        {
+            // left blank
+        }
+
+        public DbSet<AddMovieResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<AddMovieResponse>().HasData(
+                new AddMovieResponse
+                {
+                    MovieId = 1,
+                    Category = "Sports",
+                    Title = "Remeber the Titans",
+                    Year = 2000,
+                    Director = "Boaz Yakin",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "Wife",
+                    Notes = "Best Movie Ever"
+                },
+                new AddMovieResponse
+                {
+                    MovieId = 2,
+                    Category = "Action/Adventure",
+                    Title = "Pirates of the Caribbean",
+                    Year = 2003,
+                    Director = "Gore Verbinski",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+                new AddMovieResponse
+                {
+                    MovieId = 3,
+                    Category = "Sci-fi/Adventure",
+                    Title = "Interstellar",
+                    Year = 2014,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                }
+            );
+        }
+    }
+}
---
+++ Mission06_masfrod2/Models/AddMovieResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_masfrod2.Models
+{
+    public class AddMovieResponse
+    {
+        [Key]
+        [Required]
+        public int MovieId { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public ushort Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        [StringLength(25, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
+        public string Notes { get; set; }
+    }
+}
